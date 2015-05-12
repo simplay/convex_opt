4f428f0226747452c6cd864d7236cb50ba2f7a4d
simplay <michael.single@students.unibe.ch>
2015-05-13 01:55:43 +0200

    Primal Dual Solver runs runtime error free :)

--- 2/demosaicing_michael_single.m
+++ 2/demosaicing_michael_single.m
@@ -49,34 +49,34 @@ function [demosaicedImg] = demosaicing_michael_single(mosaiced, Omega, lambda, i
     demosaicedImg = mat2Img(uR,uG,uB);
 end
 
-function [y_n_p_1, x_n_p_1, x_tilde_n_p_1] = primal_dual_solver(y_n, x_n, x_tilde_n, g, omega, lambda, tau, sigma, theta)
+function [y_n_p_1, x_n_p_1, x_tilde_n_p_1] = primal_dual_solver(y_n, x_n, x_tilde_n, g, Omega, lambda, tau, sigma, theta)
 % @param y_n current partial y iterative solution of primal dual formulation.
 % @param x_n current partial x iterative solution of primal dual formulation.
 % @param x_tilde_n current iterative solution of primal dual formulation.
 % @param g initial mosaiced image of color channel corresponding to given u.
-% @param omega bayer filter mask M x N for a certain color channel
+% @param Omega bayer filter mask M x N for a certain color channel
 % @param lambda [Float] regularization factor to control exactness of result and the smoothness 
 %        when solving the demosaicing convex minimization problem.
 % @param tau learning rate for gradient descend.
 % @param eps small perturbation to avoid division by zero issues.
 % @return next iteration of color channel u.
-	y_n_p_1 = 1;
-    x_n_p_1 = 1;
-    x_tilde_n_p_1 = 1;
+	y_n_p_1 = y_n_plus_1_for(y_n, x_tilde_n, sigma);
+    x_n_p_1 = x_n_plus_1_for(x_n, y_n_p_1, lambda, tau, Omega, g);
+    x_tilde_n_p_1 = x_tilde_n_plus_1_for(x_n_p_1, x_n, theta);
 end
 
-function y_n_p_1 = y_n_plus_1_for(y_n, x_tilde)
+function y_n_p_1 = y_n_plus_1_for(y_n, x_tilde, sigma)
     grad_tilde_x = grad_of(x_tilde);
     y_nominator = y_n + sigma*grad_tilde_x;
     norm_y_nominator = sqrt(y_nominator(:,:,1).^2 + y_nominator(:,:,2).^2);
     y_denominator = max(1, norm_y_nominator);
 
     y_n_p_1 = zeros(size(y_n));
-    y_n_p_1(:,:,1) = y_nominator(:,:,1)./y_denominator(:,:,1);
-    y_n_p_1(:,:,2) = y_nominator(:,:,2)./y_denominator(:,:,2);
+    y_n_p_1(:,:,1) = y_nominator(:,:,1)./y_denominator;
+    y_n_p_1(:,:,2) = y_nominator(:,:,2)./y_denominator;
 end
 
-function x_n_p_1 = x_n_plus_1_for(x_n, y_n_p_1, lamdba, tau, Omega, g)
+function x_n_p_1 = x_n_plus_1_for(x_n, y_n_p_1, lambda, tau, Omega, g)
     div_y_n_p_1 = div_of(y_n_p_1);
     x_n_p_1_nominator = x_n + tau*div_y_n_p_1 + tau*lambda*(Omega.*g);
     x_n_p_1_denominator = 1 + tau*lambda*Omega;
@@ -91,8 +91,8 @@ function grad_f = grad_of(f)
 % foreward difference scheme
 
     grad_f = zeros([size(f), 2]);
-    df_dx = f(2:end,:)-f(1:end-1,:);
-    df_dy = f(:,2:end)-f(:,1:end-1);
+    df_dx = f([2:end, end],:)-f(:,:);
+    df_dy = f(:,[2:end, end])-f(:,:);
     grad_f(:,:,1) = df_dx;
     grad_f(:,:,2) = df_dy;
 end
